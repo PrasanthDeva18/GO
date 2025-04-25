8de6c73a8ce39219a23f95bce27bc8863c19aa5b
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-04-25 19:59:50 +0530

    Update switch.go

--- switch.go
+++ switch.go
@@ -1,1 +1,17 @@
+pacakge main
 
+import "fmt"
+
+func main(){
+  fmt.Println("Learn about Switch");
+
+  switch choice {
+    case 1:
+      fmt.Println("Print 1")
+    case 2:
+      fmt.Println("Print 2")
+    default:
+      fmt.Println("No Choice");
+    
+  }
+}
