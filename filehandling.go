36fbda4287654a1ac94a20ed71c8245841219d33
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-05-06 14:13:48 +0530

    Update filehandling.go

--- filehandling.go
+++ filehandling.go
@@ -1,33 +1,34 @@
 package main
+
 import (
-  "fmt",
-  "os",
-  "strconv"
-};
-  
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func readAFileContent() float64 {
-  data, _ := os.ReadFile('file.txt');
-  b := string(data);
-  balance, _ := strconv.parseFloat(b, 64);
-  return balance;
+	data, _ := os.ReadFile("file.txt")
+	b := string(data)
+	balance, _ := strconv.ParseFloat(b, 64)
+	return balance
 }
-  
-func writeAcontentFile(balance float64){
-  b := fmt.Sprint(balance)
-  os.WriteFile("file.txt", []byte(b), 0644)
+
+func writeAcontentFile(balance float64) {
+	b := fmt.Sprint(balance)
+	os.WriteFile("file.txt", []byte(b), 0644)
 }
 
 func main() {
-    var accountBalance = 1000;
-    var deposit float64;
-    var submit bool;
-    fmt.Scan(&deposit);
-    fmt.Scan(&submit)
+	var accountBalance float64 = 1000
+	var deposit float64
+	var submit bool
+	fmt.Scan(&deposit)
+	fmt.Scan(&submit)
 
-    if submit {
-      accountBalance += deposit;
-      writeAcontentFile(accountBalance);
-      balanceAmount := readAFileContent()
-      fmt.Println(balanceAmount)
-    }
+	if submit {
+		accountBalance += deposit
+		writeAcontentFile(accountBalance)
+		balanceAmount := readAFileContent()
+		fmt.Println(balanceAmount)
+	}
 }
