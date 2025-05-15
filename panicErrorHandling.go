8ffb433925d14f067cb733fe4942e41a258d8123
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-05-15 11:34:51 +0530

    Create panicErrorHandling.go

---
+++ panicErrorHandling.go
@@ -0,0 +1,21 @@
+pacakge main
+import (
+  "fmt"
+  "panic"
+  "errors"
+)
+
+func errorfunction()(error){
+  return errors.New("this is from error function");
+}
+
+// panic keyword usage
+func main(){
+    var err = errorfunction()
+    if err != nil {
+        fmt.Println(err);
+        // return 
+        // panic("can't continue the code") 
+      panic(err) 
+    }
+}
